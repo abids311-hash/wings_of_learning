2aa4975d19d0d5112c05ea08b64e9b133ef87cbd
abids311-hash <abids311@gmail.com>
2025-08-22 23:54:50 +0530

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,34 +1,6 @@
-import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: const Text('Go to Login Page'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/hello');
-              },
-              child: const Text('Go to Hello Page'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+ElevatedButton(
+  onPressed: () {
+    Navigator.pushNamed(context, '/hello');
+  },
+  child: const Text('Go to Hello Page'),
+),
