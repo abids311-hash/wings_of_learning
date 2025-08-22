6234bc986bb9f888d05596f21f3119c4d4ddfdfb
abids311-hash <abids311@gmail.com>
2025-08-23 02:28:52 +0530

    Add files via upload

---
+++ lib/hello.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HelloPage extends StatelessWidget {
+  const HelloPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello Page')),
+      body: const Center(
+        child: Text('Hello from Hello Page!'),
+      ),
+    );
+  }
+}
