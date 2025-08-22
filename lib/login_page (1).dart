b0b64f5ceadcc458551e347feef2e56422baba89
abids311-hash <abids311@gmail.com>
2025-08-23 02:23:24 +0530

    Add files via upload

---
+++ lib/login_page (1).dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/hello'),
+          child: const Text('Go to Hello Page'),
+        ),
+      ),
+    );
+  }
+}
