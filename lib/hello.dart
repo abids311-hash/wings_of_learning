5a0a1642e27507281506ce2d0ded89b109a9548a
abids311-hash <abids311@gmail.com>
2025-08-22 23:39:31 +0530

    Update hello.dart

--- lib/hello.dart
+++ lib/hello.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HelloPage extends StatelessWidget {
-  const HelloPage({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Hello Page")),
-      body: const Center(child: Text("Hello, welcome to Wings of Learning!")),
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/hello');
+          },
+          child: const Text('Login and Go to Hello Page'),
+        ),
+      ),
     );
   }
 }
