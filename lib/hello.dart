37c0e160d13b86e092a6b69103b683b52653150f
abids311-hash <abids311@gmail.com>
2025-08-22 23:30:10 +0530

    Update hello.dart

--- lib/hello.dart
+++ lib/hello.dart
@@ -1,3 +1,13 @@
-void main() {
-  print("Hello Abid! 🚀 Your file upload worked!");
+import 'package:flutter/material.dart';
+
+class HelloPage extends StatelessWidget {
+  const HelloPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Hello Page")),
+      body: const Center(child: Text("Hello, welcome to Wings of Learning!")),
+    );
+  }
 }
