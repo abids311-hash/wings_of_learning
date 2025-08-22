9392bdfb52924fd621a225b55de58cf0d72f2ce8
abids311-hash <abids311@gmail.com>
2025-08-23 02:09:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wings of Learning',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
       routes: {
         '/': (context) => const HomePage(),
         '/login': (context) => const LoginPage(),
         '/hello': (context) => const HelloPage(),
       },
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
     );
   }
 }
