ac34672c3c6472f411d317829b9c713ffcd07066
abids311-hash <abids311@gmail.com>
2025-08-23 01:59:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
-routes: {
-  '/': (context) => const HomePage(),
-  '/login': (context) => const LoginPage(),
-  '/hello': (context) => const HelloPage(),
-},
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'login_page.dart';
+import 'hello.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wings of Learning',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/login': (context) => const LoginPage(),
+        '/hello': (context) => const HelloPage(),
+      },
+    );
+  }
+}
